dd1c1820008f0b92d8cdcd68942203174f4a51c6
Gregory Hart <greg@freastro.net>
2019-06-19 22:45:16 -0500

    Fixed submodules for Travis-CI and publish packages

--- build-osx.sh
+++ build-osx.sh
@@ -8,4 +8,5 @@ export PATH="./node_modules/.bin:$PATH"
 
 npm install --build-from-source --clang=1 ${GYP_ARGS}
 npm install -g electron@${ELECTRON_VERSION}
-node-pre-gyp package $GYP_ARGS
+node-pre-gyp package ${GYP_ARGS}
+node-pre-gpy publish ${GYP_ARGS}
