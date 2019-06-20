d6dddfae6adbf2c748a5099319ed6d43a9934086
Gregory Hart <greg@freastro.net>
2019-06-19 23:04:38 -0500

    Fixed typo

--- build-osx.sh
+++ build-osx.sh
@@ -9,4 +9,4 @@ export PATH="./node_modules/.bin:$PATH"
 npm install --build-from-source --clang=1 ${GYP_ARGS}
 npm install -g electron@${ELECTRON_VERSION}
 node-pre-gyp package ${GYP_ARGS}
-node-pre-gpy publish ${GYP_ARGS}
+node-pre-gyp publish ${GYP_ARGS}
