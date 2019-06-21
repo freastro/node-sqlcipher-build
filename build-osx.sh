4fd0f6e4e817113a43ea509f90486d176f4e6fc0
Gregory Hart <greg@freastro.net>
2019-06-20 21:49:02 -0500

    Install sqlcipher where it can be found by node-sqlite3

--- build-osx.sh
+++ build-osx.sh
@@ -5,13 +5,17 @@ set -x
 GYP_ARGS="--runtime=electron --target=${ELECTRON_VERSION} --dist-url=https://atom.io/download/electron"
 export PATH="./node_modules/.bin:$PATH"
 
+mkdir build
+PREFIX="$(pwd)/build"
+
 pushd sqlcipher
-./configure --enable-tempstore=yes --enable-load-extension --disable-tcl CFLAGS="-DSQLITE_HAS_CODEC -DSQLITE_ENABLE_JSON1 -DSQLITE_ENABLE_FTS3 -DSQLITE_ENABLE_FTS3_PARENTHESIS -DSQLITE_ENABLE_FTS5" CPPFLAGS="-I$(brew --prefix openssl)/include" LDFLAGS="-L$(brew --prefix openssl)/lib"
+./configure --prefix="${PREFIX}" --enable-tempstore=yes --enable-load-extension --disable-tcl CFLAGS="-DSQLITE_HAS_CODEC -DSQLITE_ENABLE_JSON1 -DSQLITE_ENABLE_FTS3 -DSQLITE_ENABLE_FTS3_PARENTHESIS -DSQLITE_ENABLE_FTS5" CPPFLAGS="-I$(brew --prefix openssl)/include" LDFLAGS="-L$(brew --prefix openssl)/lib"
 make
+make install
 popd
 
 pushd node-sqlite3
-npm install --build-from-source --clang=1 --sqlite_libname=sqlcipher --sqlite=../sqlcipher/ ${GYP_ARGS}
+npm install --build-from-source --clang=1 --sqlite_libname=sqlcipher --sqlite="${PREFIX}" ${GYP_ARGS}
 node-pre-gyp package ${GYP_ARGS}
 
 sed -e s#.*\"host\":.*#\"host\":\"https://static.freastro.net.s3.amazonaws.com\",# package.json > package.json.new
